6012d6bb03317af9a7079315e24c7a39672663b2
Tienanh01 <63101309+Tienanh01@users.noreply.github.com>
2023-04-16 07:55:38 +0700

    upp

---
+++ Thang4/EverydayProject/src/main/java/com/java/EverydayProject/EverydayProjectApplication.java
@@ -0,0 +1,13 @@
+package com.java.EverydayProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverydayProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverydayProjectApplication.class, args);
+	}
+
+}
---
+++ Thang4/EverydayProject/src/test/java/com/java/EverydayProject/EverydayProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.EverydayProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverydayProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
